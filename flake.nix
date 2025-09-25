022e1c52bc6c0607751f8d4af63128cc9779d1b0
Mathieu Moalic <matmoa@pm.me>
2025-09-25 14:52:24 +0200

    Add pre-built package to flake.nix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       in rec {
         packages.trein = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
           pname = "trein";
-          version = "0.1.0";
+          version = "0.1.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
@@ -64,6 +64,39 @@
 
         checks.build = packages.trein;
         formatter = pkgs.nixpkgs-fmt;
+
+        treinPreBuiltVersion = "0.1.1";
+        treinRelease =
+          {
+            x86_64-linux = {
+              url = "https://github.com/MathieuMoalic/trein/releases/download/v${treinPreBuiltVersion}/trein-v${treinPreBuiltVersion}-linux-x86_64";
+              hash = "sha256-7tE141CuhMC6lkJlIH5Qu0ayNrx9p3NYEh5rGObmDPw=";
+            };
+            aarch64-linux = {
+              url = "https://github.com/MathieuMoalic/trein/releases/download/v${treinPreBuiltVersion}/trein-v${treinPreBuiltVersion}-linux-aarch64";
+              hash = "sha256-hGATLL2Lmjd+A6vleSBu1MB6e50dHuedx1CaC5oJ+54=";
+            };
+          }.${
+            system
+          };
+
+        packages.trein-bin = pkgs.stdenvNoCC.mkDerivation {
+          pname = "trein";
+          version = treinPreBuiltVersion;
+
+          src = pkgs.fetchurl {
+            inherit (treinRelease) url hash;
+          };
+
+          dontUnpack = true;
+          installPhase = ''
+            install -Dm755 "$src" "$out/bin/trein"
+          '';
+        };
+        apps.trein-bin = {
+          type = "app";
+          program = "${packages.trein-bin}/bin/trein";
+        };
       }
     );
 }
